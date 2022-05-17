7462f99fb7f046dc348d06c7ae53ee1448d49bd8
Philipp Krüger <philipp.krueger1@gmail.com>
2022-05-17 10:36:16 +0200

    Avoid reference equality in map keys

--- crates/wasm/examples/graph/src/blockstore.ts
+++ crates/wasm/examples/graph/src/blockstore.ts
@@ -7,7 +7,7 @@ import { sha256 } from "multiformats/hashes/sha2";
  * IPFS is basically a glorified HashMap.
  */
 export class MemoryBlockStore {
-  private store: Map<CID, Uint8Array>;
+  private store: Map<string, Uint8Array>;
 
   /** Creates a new in-memory block store. */
   constructor() {
@@ -17,13 +17,13 @@ export class MemoryBlockStore {
   /** Stores an array of bytes in the block store. */
   async getBlock(cid: Uint8Array): Promise<Uint8Array | undefined> {
     const decoded_cid = CID.decode(cid);
-    return this.store.get(decoded_cid);
+    return this.store.get(decoded_cid.toString());
   }
 
   /** Retrieves an array of bytes from the block store with given CID. */
   async putBlock(bytes: Uint8Array, code: number): Promise<void> {
     const hash = await sha256.digest(bytes);
     const cid = CID.create(1, code, hash);
-    this.store.set(cid, bytes);
+    this.store.set(cid.toString(), bytes);
   }
 }
