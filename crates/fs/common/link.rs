3a6c54d53d4a68fc5f723df5579104d5dff442b8
Philipp Krüger <philipp.krueger1@gmail.com>
2022-06-03 13:31:43 +0200

    Rename `IpldLink` into `Link`

--- crates/fs/common/link.rs
+++ crates/fs/common/link.rs
@@ -7,7 +7,7 @@ use libipld::{cbor::DagCborCodec, codec::Decode, Cid};
 use crate::blockstore;
 use crate::BlockStore;
 
-pub(crate) enum IpldLink<T> {
+pub(crate) enum Link<T> {
     /// Invariant: the (optional) contents of the cache *must* encode the cid
     Clean {
         cid: Cid,
@@ -16,7 +16,7 @@ pub(crate) enum IpldLink<T> {
     Dirty(T),
 }
 
-impl<T> IpldLink<T>
+impl<T> Link<T>
 where
     T: Decode<DagCborCodec> + Encode<DagCborCodec> + OptimizedAwayValue,
 {
@@ -26,12 +26,12 @@ where
 
     async fn resolve<B: BlockStore>(&self, store: &B) -> Result<&T> {
         match self {
-            IpldLink::Clean { cid, cache } => {
+            Self::Clean { cid, cache } => {
                 cache
                     .get_or_try_init(async { blockstore::load(store, cid).await })
                     .await
             }
-            IpldLink::Dirty(node) => Ok(node),
+            Self::Dirty(node) => Ok(node),
         }
     }
 
@@ -51,18 +51,18 @@ where
 
     async fn get<B: BlockStore>(self, store: &B) -> Result<T> {
         match self {
-            IpldLink::Clean { cid, cache } => match cache.into_inner() {
+            Self::Clean { cid, cache } => match cache.into_inner() {
                 Some(cached) => Ok(cached),
                 None => blockstore::load(store, &cid).await,
             },
-            IpldLink::Dirty(node) => Ok(node),
+            Self::Dirty(node) => Ok(node),
         }
     }
 
     async fn seal<B: BlockStore>(&mut self, store: &mut B) -> Result<Cid> {
         match self {
-            IpldLink::Clean { cid, .. } => Ok(*cid),
-            IpldLink::Dirty(node) => {
+            Self::Clean { cid, .. } => Ok(*cid),
+            Self::Dirty(node) => {
                 let mut bytes: Vec<u8> = Vec::new();
                 node.encode(DagCborCodec, &mut bytes)?;
                 let cid = store.put_block(bytes, IpldCodec::DagCbor).await?;
@@ -75,18 +75,40 @@ where
             }
         }
     }
+
+    fn is_clean(&self) -> bool {
+        matches!(self, Self::Clean { .. })
+    }
+
+    fn is_cached(&self) -> bool {
+        match self {
+            Self::Clean { cache, .. } => cache.get().is_some(),
+            Self::Dirty(_) => true,
+        }
+    }
 }
 
 #[cfg(test)]
 mod ipld_link_tests {
-    use crate::{IpldLink, MemoryBlockStore, OptimizedAwayValue};
+    use crate::{Link, MemoryBlockStore, OptimizedAwayValue};
 
     #[async_std::test]
     async fn ipld_link() {
         let link = IpldLink::new(10_u64);
+        let mut link = Link::to(42_u64);
         let mut store = MemoryBlockStore::default();
         let cid = link.seal(&mut store).await.unwrap();
-        println!("{}", cid)
+        println!("Clean? {}", link.is_clean());
+        println!("{}", cid);
+
+        // another link
+        let link2: Link<u64> = Link::from_cid(cid);
+        println!("Clean? {} Cached? {}", link2.is_clean(), link2.is_cached());
+        let num = *link2.resolve(&store).await.unwrap();
+        println!("num: {num}");
+        // interior mutability makes is_cached suddenly return true :S
+        // we may want to just never have that be observable behavior from the outside.
+        println!("Clean? {} Cached? {}", link2.is_clean(), link2.is_cached());
     }
 
     impl OptimizedAwayValue for u64 {
