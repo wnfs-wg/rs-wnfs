14aa9bcad103cd187eb0124e61a1bce02887148f
Philipp Krüger <philipp.krueger1@gmail.com>
2022-08-19 11:32:11 +0200

    Expose setting the panic hook

--- crates/wasm/lib.rs
+++ crates/wasm/lib.rs
@@ -12,6 +12,7 @@ pub mod fs;
 ///
 /// This function needs to be called at least once during initialisation.
 /// https://rustwasm.github.io/docs/wasm-pack/tutorials/npm-browser-packages/template-deep-dive/src-utils-rs.html#2-what-is-console_error_panic_hook
+#[wasm_bindgen(js_name = "setPanicHook")]
 pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
