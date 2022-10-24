3a4b012fc8bfd12cef4ef589399c76a9788e7cb7
Stephen Akinyemi <appcypher@outlook.com>
2022-10-24 11:56:09 +0100

    Add suggestions by @zeeshanlakhani
    - Unrestrict version path
    - Bench throughput
    - Add github action

--- crates/fs/benches/hamt.rs
+++ crates/fs/benches/hamt.rs
@@ -1,7 +1,7 @@
 use criterion::{
     async_executor::AsyncStdExecutor, black_box, criterion_group, criterion_main, Criterion,
+    Throughput,
 };
-use libipld::Cid;
 use std::rc::Rc;
 use wnfs::{
     dagcbor,
@@ -24,7 +24,7 @@ fn node_set(c: &mut Criterion) {
 
 fn node_load_get(c: &mut Criterion) {
     let mut store = MemoryBlockStore::default();
-    let cid: Cid = async_std::task::block_on(async {
+    let (cid, bytes) = async_std::task::block_on(async {
         let mut node = Rc::new(<Node<_, _>>::default());
         for i in 0..50 {
             node = node.set(i.to_string(), i, &mut store).await.unwrap();
@@ -33,10 +33,15 @@ fn node_load_get(c: &mut Criterion) {
         let encoded_hamt = dagcbor::async_encode(&Hamt::with_root(node), &mut store)
             .await
             .unwrap();
-        store.put_serializable(&encoded_hamt).await.unwrap()
+
+        let cid = store.put_serializable(&encoded_hamt).await.unwrap();
+
+        (cid, encoded_hamt)
     });
 
-    c.bench_function("Node load and get", |b| {
+    let mut group = c.benchmark_group("With throughput");
+    group.throughput(Throughput::Bytes(bytes.len() as u64));
+    group.bench_function("Node load and get", |b| {
         b.to_async(AsyncStdExecutor).iter(|| async {
             let encoded_hamt = store.get_deserializable::<Vec<u8>>(&cid).await.unwrap();
             let hamt: Hamt<String, i32> = dagcbor::decode(encoded_hamt.as_ref()).unwrap();
@@ -51,11 +56,13 @@ fn node_load_get(c: &mut Criterion) {
             }
         })
     });
+
+    group.finish();
 }
 
 fn node_load_remove(c: &mut Criterion) {
     let mut store = MemoryBlockStore::default();
-    let cid: Cid = async_std::task::block_on(async {
+    let (cid, bytes) = async_std::task::block_on(async {
         let mut node = Rc::new(<Node<_, _>>::default());
         for i in 0..50 {
             node = node.set(i.to_string(), i, &mut store).await.unwrap();
@@ -65,10 +72,14 @@ fn node_load_remove(c: &mut Criterion) {
             .await
             .unwrap();
 
-        store.put_serializable(&encoded_hamt).await.unwrap()
+        let cid = store.put_serializable(&encoded_hamt).await.unwrap();
+
+        (cid, encoded_hamt)
     });
 
-    c.bench_function("Node load and remove", |b| {
+    let mut group = c.benchmark_group("With throughput");
+    group.throughput(Throughput::Bytes(bytes.len() as u64));
+    group.bench_function("Node load and remove", |b| {
         b.to_async(AsyncStdExecutor).iter(|| async {
             let encoded_hamt = store.get_deserializable::<Vec<u8>>(&cid).await.unwrap();
             let mut hamt: Hamt<String, i32> =
@@ -83,43 +94,26 @@ fn node_load_remove(c: &mut Criterion) {
     });
 }
 
-macro_rules! hamt {
-    ($store:expr, { $( ($key:expr, $value:expr) $(, ($key_rest:expr, $value_rest:expr) )* $(,)? )? }) => {{
-        let root = Rc::new(Node::default());
-        $(
-            let root = root.set($key, $value, $store).await.unwrap();
-            $(
-                let root = root.set($key_rest, $value_rest, $store).await.unwrap();
-            )*
-        )?
-        black_box(Hamt::with_root(root))
-    }};
-}
-
 fn hamt_load_decode(c: &mut Criterion) {
     let mut store = MemoryBlockStore::default();
-    let cid: Cid = async_std::task::block_on(async {
-        let hamt: Hamt<String, _> = hamt!(&mut store, {
-            ("foo".into(), 1),
-            ("bar".into(), 2),
-            ("baz".into(), 3),
-            ("qux".into(), 4),
-            ("quux".into(), 5),
-            ("corge".into(), 6),
-            ("grault".into(), 7),
-            ("garply".into(), 8),
-            ("waldo".into(), 9),
-            ("fred".into(), 10),
-            ("plugh".into(), 11),
-            ("xyzzy".into(), 12),
-            ("thud".into(), 13),
-        });
+    let (cid, bytes) = async_std::task::block_on(async {
+        let mut node = Rc::new(<Node<_, _>>::default());
+        for i in 0..50 {
+            node = node.set(i.to_string(), i, &mut store).await.unwrap();
+        }
 
-        let encoded_hamt = dagcbor::async_encode(&hamt, &mut store).await.unwrap();
-        store.put_serializable(&encoded_hamt).await.unwrap()
+        let encoded_hamt = dagcbor::async_encode(&Hamt::with_root(node), &mut store)
+            .await
+            .unwrap();
+
+        let cid = store.put_serializable(&encoded_hamt).await.unwrap();
+
+        (cid, encoded_hamt)
     });
 
-    c.bench_function("HAMT load and decode", |b| {
+    let mut group = c.benchmark_group("With throughput");
+    group.throughput(Throughput::Bytes(bytes.len() as u64));
+    group.bench_function("HAMT load and decode", |b| {
         b.to_async(AsyncStdExecutor).iter(|| async {
             let encoded_hamt = store.get_deserializable::<Vec<u8>>(&cid).await.unwrap();
             let _: Hamt<String, i32> = black_box(dagcbor::decode(encoded_hamt.as_ref()).unwrap());
@@ -131,21 +125,12 @@ fn hamt_set_encode(c: &mut Criterion) {
     c.bench_function("HAMT set and encode", |b| {
         b.to_async(AsyncStdExecutor).iter(|| async {
             let store = &mut MemoryBlockStore::default();
-            let hamt: Hamt<String, _> = hamt!(store, {
-                ("foo".into(), 1),
-                ("bar".into(), 2),
-                ("baz".into(), 3),
-                ("qux".into(), 4),
-                ("quux".into(), 5),
-                ("corge".into(), 6),
-                ("grault".into(), 7),
-                ("garply".into(), 8),
-                ("waldo".into(), 9),
-                ("fred".into(), 10),
-                ("plugh".into(), 11),
-                ("xyzzy".into(), 12),
-                ("thud".into(), 13),
-            });
+            let mut node = Rc::new(<Node<_, _>>::default());
+            for i in 0..50 {
+                node = node.set(i.to_string(), i, store).await.unwrap();
+            }
+
+            let hamt = Hamt::with_root(node);
 
             let _ = black_box(dagcbor::async_encode(&hamt, store).await.unwrap());
         })
