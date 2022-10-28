f0f8b18ae8c446f1473e3724b61ef0c3be9e6bd4
github-action-benchmark <github@users.noreply.github.com>
2022-10-28 23:17:34 +0000

    add Rust Benchmark (cargo) benchmark result for b00eed1503d7641885df026a733c67ac542a8ed4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666779592012,
+  "lastUpdate": 1666999054691,
   "repoUrl": "https://github.com/wnfs-wg/rs-wnfs",
   "entries": {
     "Rust Benchmark": [
@@ -55,6 +55,90 @@ window.BENCHMARK_DATA = {
             "name": "namefilter add",
             "value": 9383,
             "range": "± 478",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "appcypher@outlook.com",
+            "name": "Stephen",
+            "username": "appcypher"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b00eed1503d7641885df026a733c67ac542a8ed4",
+          "message": "Initial Benchmark Work (#75)\n\n* Add hamt and namefilter benchmarks\r\n\r\n* Move criterion to dev-deps\r\n\r\n* Add suggestions by @zeeshanlakhani\r\n\r\n- Unrestrict version path\r\n- Bench throughput\r\n- Add github action\r\n\r\n* Fix cargo bench extra args issue\r\n\r\n* Add throughput in namefilter bench\r\n\r\n* Update logo link\r\n\r\n* Move files after rebase\r\n\r\n* Change relative readme links to absolute\r\n\r\n* Adjust namefilter benchmarks & add Sampleable\r\n\r\nCo-authored-by: Stephen <appcypher@users.noreply.github.com>\r\n\r\n* Extract out benchmarks into its own crate\r\n\r\nCo-authored-by: Stephen <appcypher@users.noreply.github.com>\r\n\r\n* Sort imports & fix bench github action\r\n\r\n* Hamt set operation doesn't need &mut store\r\n\r\n* Setup big random HAMT before node set\r\n\r\n* Fix CI issues\r\n\r\n* More fixes\r\n\r\n* Remove unnecessary throughput\r\n\r\n* Add docs, link to gh page, etc.\r\n\r\nCo-authored-by: Philipp Krüger <philipp.krueger1@gmail.com>\r\nCo-authored-by: Stephen <appcypher@users.noreply.github.com>",
+          "timestamp": "2022-10-29T00:08:12+01:00",
+          "tree_id": "969b0ea2d4cae8c243db6345d3b50f7f244e7858",
+          "url": "https://github.com/wnfs-wg/rs-wnfs/commit/b00eed1503d7641885df026a733c67ac542a8ed4"
+        },
+        "date": 1666999053626,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "node set",
+            "value": 9035,
+            "range": "± 247",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node load and get",
+            "value": 177854,
+            "range": "± 4632",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node load and remove",
+            "value": 316227,
+            "range": "± 4025",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "hamt load and decode/0",
+            "value": 41947,
+            "range": "± 1338",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "hamt set and encode",
+            "value": 305405,
+            "range": "± 7549",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter add",
+            "value": 9344,
+            "range": "± 287",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter contains",
+            "value": 13138,
+            "range": "± 263",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter saturate",
+            "value": 48996,
+            "range": "± 1394",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter encode",
+            "value": 193,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter decode/0",
+            "value": 2,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
