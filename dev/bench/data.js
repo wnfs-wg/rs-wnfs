0b01c404825281a281b7761aa07b52c3234cb8b2
github-action-benchmark <github@users.noreply.github.com>
2022-11-02 13:31:21 +0000

    add Rust Benchmark (cargo) benchmark result for 7a187bc05fd11c14b14099145a6b81d6e810d3a1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666999054691,
+  "lastUpdate": 1667395881029,
   "repoUrl": "https://github.com/wnfs-wg/rs-wnfs",
   "entries": {
     "Rust Benchmark": [
@@ -138,6 +138,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "namefilter decode/0",
             "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "philipp.krueger1@gmail.com",
+            "name": "Philipp Krüger",
+            "username": "matheus23"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7a187bc05fd11c14b14099145a6b81d6e810d3a1",
+          "message": "Make private forest multivalue (#78)\n\n* Make private forest multivalue\r\n\r\n* Write own implementation of `BTreeSet::first`\r\n\r\nit's not in stable yet (CI).\r\nAlso added a test\r\n\r\n* Remove const `BTreeSet::new`, as it's also unstable\r\n\r\n* Make lil' clippy happy\r\n\r\n* Fix wasm tests\r\n\r\n* Remove unused `BTreeSet` imports",
+          "timestamp": "2022-11-02T14:25:30+01:00",
+          "tree_id": "541515509f8d7a61b6778155cebb1223e04b6421",
+          "url": "https://github.com/wnfs-wg/rs-wnfs/commit/7a187bc05fd11c14b14099145a6b81d6e810d3a1"
+        },
+        "date": 1667395879848,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "node set",
+            "value": 7150,
+            "range": "± 84",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node load and get",
+            "value": 161906,
+            "range": "± 456",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node load and remove",
+            "value": 272648,
+            "range": "± 864",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "hamt load and decode/0",
+            "value": 40890,
+            "range": "± 187",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "hamt set and encode",
+            "value": 264432,
+            "range": "± 2298",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter add",
+            "value": 7758,
+            "range": "± 152",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter contains",
+            "value": 10762,
+            "range": "± 52",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter saturate",
+            "value": 42548,
+            "range": "± 46",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter encode",
+            "value": 155,
+            "range": "± 9",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "namefilter decode/0",
+            "value": 1,
             "range": "± 0",
             "unit": "ns/iter"
           }
