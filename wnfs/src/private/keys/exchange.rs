28aa3399bf8eec56aae9a8dd3baebe3de300556d
Stephen Akinyemi <appcypher@outlook.com>
2023-06-12 15:28:28 +0100

    Fix import

--- wnfs/src/private/keys/exchange.rs
+++ wnfs/src/private/keys/exchange.rs
@@ -5,7 +5,7 @@ use anyhow::anyhow;
 use anyhow::Result;
 use async_trait::async_trait;
 #[cfg(test)]
-use rsa::{BigUint, Oaep, PublicKey as PublicKeyTrait, PublicKeyParts};
+use rsa::{BigUint, Oaep, traits::PublicKeyParts};
 #[cfg(test)]
 use sha2::Sha256;
 
